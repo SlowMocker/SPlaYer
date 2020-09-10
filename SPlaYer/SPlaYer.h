997dd393919af6ab8533e1fbef01c38059ce1b4f
wuwenhao <wenhao.wu@mosi-tech.com>
2020-09-10 10:25:22 +0800

    init

---
+++ SPlaYer/SPlaYer.h
@@ -0,0 +1,13 @@
+//
+//  SPlaYer.h
+//  SPlaYer
+//
+//  Created by iSmicro on 2020/9/10.
+//  Copyright © 2020 iSmicro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPlaYer : NSObject
+
+@end
---
+++ SPlaYer/SPlaYer.m
@@ -0,0 +1,13 @@
+//
+//  SPlaYer.m
+//  SPlaYer
+//
+//  Created by iSmicro on 2020/9/10.
+//  Copyright © 2020 iSmicro. All rights reserved.
+//
+
+#import "SPlaYer.h"
+
+@implementation SPlaYer
+
+@end
